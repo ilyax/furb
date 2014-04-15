acf561989066c6d7e75afb73505669b488e5d3ac
iLyax <ilyas.osmanoglu@gmail.com>
2014-04-15 17:02:42 +0300

    corp test

--- irrelevant/run.bat
+++ irrelevant/run.bat
@@ -1,3 +1,8 @@
- echo off
+REM  Furb
+REM 
+REM  Copyright © Ilyas Kolasinac Osmanogullari, 2014
+REM  ilyax@outlook.com || http://www.ilyax.com
+
+echo off
 cls
 "C:\Program Files\nodejs\node.exe" "C:\MongoChat\furb.js"
